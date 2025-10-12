2d7d93e1b6ecaf3ef93764600408c07d6b7aa223
smw0807 <smw0807@gmail.com>
2025-10-13 07:26:16 +0900

    feat: 검색필터 셀렉트박스 추가

---
+++ app/models/common.ts
@@ -0,0 +1,4 @@
+export type GameType = 'all' | 'ranked' | 'unranked';
+export type GameMode = 'all' | 'duo' | 'squad';
+
+export type Platform = 'kakao' | 'steam';
--- app/pages/teams/[platform].vue
+++ app/pages/teams/[platform].vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
 import { mockTeams } from '~/assets/mock/temas';
+import type { GameMode, GameType } from '~/models/common';
 import { platformTextTransform } from '~/utils/textTransform';
 
 const { platform } = useRoute().params as { platform: string };
+
+const selectedGameType = ref<GameType>('all');
+const gameTypeOptions = [
+  { label: '전체', value: 'all' },
+  { label: '랭크', value: 'ranked' },
+  { label: '언랭크', value: 'unranked' },
+];
+const selectedGameMode = ref<GameMode>('all');
+const gameModeOptions = [
+  { label: '전체', value: 'all' },
+  { label: '듀오', value: 'duo' },
+  { label: '스쿼드', value: 'squad' },
+];
 
 const handleClick = (id: number) => {
   console.log(id);
@@ -16,13 +30,31 @@ const handleClick = (id: number) => {
       </h1>
     </div>
 
-    <div class="flex items-center justify-end gap-4 mb-4">
-      <!-- 방 리스트 새로고침 버튼 아이콘 -->
-      <UButton color="neutral" size="lg" variant="ghost">
-        <UIcon name="i-heroicons-arrow-path-20-solid" class="w-6 h-6" />
-      </UButton>
-      <!-- 방 만들기 버튼 -->
-      <UButton color="primary" size="lg">방 만들기</UButton>
+    <div class="flex items-center justify-between gap-4 mb-4">
+      <div class="flex items-center gap-4">
+        <USelect
+          v-model="selectedGameType"
+          :items="gameTypeOptions"
+          option-attribute="label"
+          value-attribute="value"
+          class="w-30"
+        />
+        <USelect
+          v-model="selectedGameMode"
+          :items="gameModeOptions"
+          option-attribute="label"
+          value-attribute="value"
+          class="w-30"
+        />
+      </div>
+      <div class="flex items-center">
+        <!-- 방 리스트 새로고침 버튼 아이콘 -->
+        <UButton color="neutral" size="lg" variant="ghost">
+          <UIcon name="i-heroicons-arrow-path-20-solid" class="w-6 h-6" />
+        </UButton>
+        <!-- 방 만들기 버튼 -->
+        <UButton color="primary" size="lg">방 만들기</UButton>
+      </div>
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
