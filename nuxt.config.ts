fae37b7a08aeb547a71345bec7846fcf29fc9b3a
smw0807 <smw0807@gmail.com>
2025-10-14 06:47:30 +0900

    feat: runtimeConfig firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,16 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  runtimeConfig: {
+    public: {
+      apiKey: process.env.FB_API_KEY,
+      authDomain: process.env.FB_AUTH_DOMAIN,
+      projectId: process.env.FB_PROJECT_ID,
+      storageBucket: process.env.FB_STORAGE_BUCKET,
+      messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+      appId: process.env.FB_APP_ID,
+      measurementId: process.env.FB_MEASUREMENT_ID,
+    },
+  },
 });
