247f481dc787e9414d402999b630e66b4abbce11
smw0807 <smw0807@gmail.com>
2025-10-14 23:43:42 +0900

    refactor: 버튼 추가 및 수정 로그아웃 버튼 수정 프로필 버튼 추가

--- app/components/MainHeader.vue
+++ app/components/MainHeader.vue
@@ -15,12 +15,17 @@ const handleSignOut = async () => {
   <UHeader title="PUBG Your Team">
     <template #right>
       <template v-if="user">
+        <UButton color="info" variant="ghost">
+          <UIcon name="i-heroicons-user-circle" class="w-6 h-6" />
+        </UButton>
         <UUser
           :name="user?.displayName as string"
           :description="user?.email as string"
           :avatar="{ src: user?.photoURL as string }"
         />
-        <UButton color="info" @click="handleSignOut">Logout</UButton>
+        <UButton color="warning" variant="ghost" @click="handleSignOut">
+          <UIcon name="i-heroicons-arrow-right-on-rectangle" class="w-6 h-6" />
+        </UButton>
       </template>
       <template v-if="!user">
         <UButton color="info" @click="handleSignIn">Login</UButton>
