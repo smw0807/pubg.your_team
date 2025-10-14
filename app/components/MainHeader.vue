1e0823d712372cb226242372d74d6f1d70297c67
smw0807 <smw0807@gmail.com>
2025-10-14 22:34:31 +0900

    feat: 로그인 로그아웃

--- app/components/MainHeader.vue
+++ app/components/MainHeader.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useAuth from '~/composables/useAuth';
+const { signIn, user, signOut } = useAuth();
+
+const handleSignIn = async () => {
+  await signIn();
+};
+
+const handleSignOut = async () => {
+  await signOut();
+};
+</script>
 
 <template>
   <UHeader title="PUBG Your Team">
     <template #right>
-      <UButton color="info">Login</UButton>
+      <template v-if="user">
+        <UUser
+          :name="user?.displayName as string"
+          :description="user?.email as string"
+          :avatar="{ src: user?.photoURL as string }"
+        />
+        <UButton color="info" @click="handleSignOut">Logout</UButton>
+      </template>
+      <template v-if="!user">
+        <UButton color="info" @click="handleSignIn">Login</UButton>
+      </template>
     </template>
 
     <NavigationMenu />
--- app/composables/useAuth.ts
+++ app/composables/useAuth.ts
@@ -3,9 +3,11 @@ import {
   getAdditionalUserInfo,
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithPopup,
   signOut,
   type Auth,
+  type User,
   type UserCredential,
 } from 'firebase/auth';
 
@@ -14,9 +16,17 @@ export default function useAuth() {
   const auth: Auth = getAuth(app);
   const provider = new GoogleAuthProvider();
 
+  const userInfo = ref<User | null>(null);
+  const user = computed<User | null>(() => userInfo.value);
+
+  // 로그인
   const signIn = async () => {
-    const result = await signInWithPopup(auth, provider);
-    console.log(result);
+    await signInWithPopup(auth, provider);
+  };
+
+  // 로그아웃
+  const handleSignOut = async () => {
+    await signOut(auth);
   };
 
   // 첫 로그인 여부 확인
@@ -25,9 +35,35 @@ export default function useAuth() {
     return result?.isNewUser || null;
   };
 
+  // 현재 로그인 사용자 확인
+  const currentUser = async (callback: (user: User | null) => void) => {
+    try {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          callback(user);
+        } else {
+          callback(null);
+        }
+      });
+    } catch (error) {
+      callback(null);
+      console.error(error);
+      throw new Error('현재 사용자 정보 가져오기 실패');
+    }
+  };
+
+  currentUser((u) => {
+    if (u) {
+      userInfo.value = u;
+    } else {
+      userInfo.value = null;
+    }
+  });
+
   return {
     signIn,
-    signOut: signOut(auth),
+    signOut: handleSignOut,
     isNewUser,
+    user,
   };
 }
