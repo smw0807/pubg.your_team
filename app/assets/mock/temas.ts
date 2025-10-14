bb11a7ba4d353e053142de2139b11a7a49cdffe2
smw0807 <smw0807@gmail.com>
2025-10-14 23:14:07 +0900

    refactor: 팀 데이터를 위한 수정

--- app/assets/mock/temas.ts
+++ app/assets/mock/temas.ts
@@ -3,7 +3,6 @@ import type { Team } from '~/models/team';
 export const mockTeams: Team[] = [
   // Bronze 티어
   {
-    id: 1,
     title: '브론즈 초보 환영',
     description: '처음 시작하시는 분들 환영합니다. 함께 성장해요!',
     type: 'squad',
@@ -14,7 +13,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3'],
   },
   {
-    id: 2,
     title: '브론즈 즐겜팟',
     description: '티어 상관없이 재밌게 즐겨요',
     type: 'duo',
@@ -27,7 +25,6 @@ export const mockTeams: Team[] = [
 
   // Silver 티어
   {
-    id: 3,
     title: '실버 올시즌만',
     description: '실버 티어만 받습니다. 천천히 배워가요',
     type: 'squad',
@@ -38,7 +35,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3'],
   },
   {
-    id: 4,
     title: '실버+ 평딜100 이상',
     description: '실버 이상, 평딜 100 이상 구합니다',
     type: 'squad',
@@ -51,7 +47,6 @@ export const mockTeams: Team[] = [
 
   // Gold 티어
   {
-    id: 5,
     title: '골드 성장팟',
     description: '플레티넘 목표로 함께 성장하실 분',
     type: 'squad',
@@ -62,7 +57,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3', '4'],
   },
   {
-    id: 6,
     title: '골드+ 평딜170',
     description: '골드 이상, 평딜 170 이상만 받습니다',
     type: 'squad',
@@ -75,7 +69,6 @@ export const mockTeams: Team[] = [
 
   // Platinum 티어
   {
-    id: 7,
     title: '플레티넘 평딜200+',
     description: '플레티넘 이상, 평딜 200 이상만 받습니다',
     type: 'squad',
@@ -86,7 +79,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2'],
   },
   {
-    id: 8,
     title: '플레+ 스쿼드',
     description: '플레티넘 이상 실력자 구합니다. 커뮤 필수',
     type: 'squad',
@@ -99,7 +91,6 @@ export const mockTeams: Team[] = [
 
   // Diamond 티어
   {
-    id: 9,
     title: '다이아 평딜300+',
     description: '다이아몬드 티어, 평딜 300 이상 구합니다',
     type: 'squad',
@@ -110,7 +101,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3', '4'],
   },
   {
-    id: 10,
     title: '다이아+ 랭크업팟',
     description: '다이아 이상 랭크업 목표. 실력자만',
     type: 'squad',
@@ -123,7 +113,6 @@ export const mockTeams: Team[] = [
 
   // Master 티어
   {
-    id: 11,
     title: '마스터 평딜350+',
     description: '마스터 티어, 평딜 350 이상 구합니다',
     type: 'squad',
@@ -134,7 +123,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3', '4'],
   },
   {
-    id: 12,
     title: '마스터+ 하드코어',
     description: '마스터 이상만. 진지하게 할 분만 오세요',
     type: 'duo',
@@ -147,7 +135,6 @@ export const mockTeams: Team[] = [
 
   // Crystal 티어
   {
-    id: 13,
     title: '크리스탈 평딜400+',
     description: '크리스탈 티어, 평딜 400 이상만 받습니다',
     type: 'squad',
@@ -158,7 +145,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2'],
   },
   {
-    id: 14,
     title: '크리스탈+ 토너먼트',
     description: '토너먼트 준비하실 분. 크리스탈 이상',
     type: 'squad',
@@ -171,7 +157,6 @@ export const mockTeams: Team[] = [
 
   // Survivor 티어
   {
-    id: 15,
     title: '서바이버 평딜500+',
     description: '서바이버 티어, 평딜 500 이상 구합니다',
     type: 'squad',
@@ -182,7 +167,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3', '4'],
   },
   {
-    id: 16,
     title: '서바이버 프로팀',
     description: '서바이버 최상위 실력자만. 대회 목적',
     type: 'squad',
@@ -195,7 +179,6 @@ export const mockTeams: Team[] = [
 
   // 티어 무관 팟
   {
-    id: 17,
     title: '즐겜팟 누구나 환영',
     description: '티어, 평딜 상관없이 재밌게 즐기실 분',
     type: 'squad',
@@ -206,7 +189,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3', '4'],
   },
   {
-    id: 18,
     title: '친목 스쿼드',
     description: '실력보다 매너와 친목 중시합니다',
     type: 'squad',
@@ -217,7 +199,6 @@ export const mockTeams: Team[] = [
     members: ['1', '2', '3'],
   },
   {
-    id: 19,
     title: '뉴비 환영 팟',
     description: '초보자 환영! 알려드립니다',
     type: 'duo',
@@ -228,7 +209,6 @@ export const mockTeams: Team[] = [
     members: ['1'],
   },
   {
-    id: 20,
     title: '대학생 스쿼드',
     description: '20대 대학생끼리 즐겜해요',
     type: 'squad',
