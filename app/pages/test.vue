1f9a10555f51e04cfdd572f11ba90a52ed7232d3
smw0807 <smw0807@gmail.com>
2025-10-14 23:08:25 +0900

    test: 테스트 데이터

---
+++ app/pages/test.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { addDoc, collection, getFirestore } from 'firebase/firestore';
+import { mockTeams } from '~/assets/mock/temas';
+import useFirebase from '~/utils/firebase';
+const handleAddTeam = () => {
+  const { app } = useFirebase();
+  const db = getFirestore(app);
+  console.log('방 데이터 추가하기');
+  mockTeams.forEach(async (team) => {
+    await addDoc(collection(db, 'teams'), team);
+  });
+};
+</script>
+
+<template>
+  <div>
+    <UButton @click="handleAddTeam">방 데이터 추가하기</UButton>
+  </div>
+</template>
