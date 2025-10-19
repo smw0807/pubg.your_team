54ccd4bb5eb047c5dbd29c9104fc14ef438dc44d
smw0807 <smw0807@gmail.com>
2025-10-19 18:10:26 +0900

    feat: 팀 방 레이아웃

--- app/composables/useRoom.ts
+++ app/composables/useRoom.ts
@@ -1,3 +1,24 @@
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
+import useFirebase from '~/utils/firebase';
+import { teamsCollection } from '~/constants/collections';
+import type { Team } from '~/models/team';
+
 export default function useRoom() {
-  return {};
+  const { app } = useFirebase();
+  const db = getFirestore(app);
+
+  // 팀 정보
+  const getTeam = async (id: string) => {
+    const data = await getDoc(doc(db, teamsCollection, id));
+    return {
+      id: data.id,
+      ...(data.data() as Team),
+    } as Team;
+  };
+
+  // 팀 나가기
+
+  return {
+    getTeam,
+  };
 }
--- app/pages/room/[id].vue
+++ app/pages/room/[id].vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
+import type { Team } from '~/models/team';
+
 const { id } = useRoute().params as { id: string };
+const { getTeam } = useRoom();
+
+const team = ref<Team | null>(null);
+
+onMounted(async () => {
+  team.value = await getTeam(id);
+  console.log(team.value);
+});
+
+const handleLeaveTeam = () => {
+  console.log('팀 나가기');
+};
 </script>
 
 <template>
   <div class="container mx-auto px-4 py-8">
-    <h1 class="text-2xl font-bold">팀 상세 : {{ id }}</h1>
+    <div class="flex items-center justify-between mb-10">
+      <div class="flex flex-col items-center gap-2">
+        <h1 class="text-2xl font-bold">{{ team?.title }}</h1>
+        <span class="text-sm text-gray-400">{{ team?.description }}</span>
+      </div>
+
+      <UBadge
+        color="error"
+        variant="outline"
+        class="cursor-pointer"
+        size="xl"
+        @click="handleLeaveTeam"
+        >팀 나가기</UBadge
+      >
+    </div>
+    <!-- 왼쪽은 접속자 / 오른쪽은 채팅 -->
+    <div class="flex items-center justify-between">
+      <div class="w-1/4">
+        <h2 class="text-lg font-bold">접속자</h2>
+      </div>
+      <div class="w-3/4">
+        <h2 class="text-lg font-bold">채팅</h2>
+      </div>
+    </div>
   </div>
 </template>
