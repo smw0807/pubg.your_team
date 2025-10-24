7a9f482dae6cbeb2a3f80836a4288f4585d9620f
smw0807 <smw0807@gmail.com>
2025-10-24 23:18:02 +0900

    feat: vercel/analytics

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import useAlert from '~/composables/useAlert';
 import useConfirm from '~/composables/useConfirm';
+import { Analytics } from '@vercel/analytics/nuxt';
 
 const { alertOpen } = useAlert();
 const { confirmOpen } = useConfirm();
@@ -12,5 +13,6 @@ const { confirmOpen } = useConfirm();
       <ModalCustomAlert :open="alertOpen" />
       <ModalCustomConfirm :open="confirmOpen" />
     </NuxtLayout>
+    <Analytics />
   </div>
 </template>
